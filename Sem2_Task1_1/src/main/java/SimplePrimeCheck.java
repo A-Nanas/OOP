9865c427fe6653ad4b5342758b7a74f82b4fc756
A-Nanas <is.nanas21@gmail.com>
2021-05-21 12:07:04 +0700

    task_2_1_1

---
+++ Sem2_Task1_1/src/main/java/SimplePrimeCheck.java
@@ -0,0 +1,16 @@
+public class SimplePrimeCheck {
+    //Our prime numbers "flag".
+    static boolean hasNotPrime = false;
+
+    public static boolean sequentialRun(long[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (Validation.isNotPrime(array[i])) {
+                hasNotPrime = true;
+                break;
+            }
+        }
+        return hasNotPrime;
+    }
+}
+
+
