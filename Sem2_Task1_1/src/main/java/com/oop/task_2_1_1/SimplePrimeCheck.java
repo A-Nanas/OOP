756c0bd2c424e82b4d7ab17ddf2cb39659d0f1ac
A-Nanas <a.istomina@g.nsu.ru>
2021-06-28 20:34:38 +0700

    commit

---
+++ Sem2_Task1_1/src/main/java/com/oop/task_2_1_1/SimplePrimeCheck.java
@@ -0,0 +1,19 @@
+package com.oop.task_2_1_1;
+
+// Sequential run of prime check.
+
+public class SimplePrimeCheck {
+    //Prime numbers flag.
+    static boolean hasNotPrime = false;
+
+    public static boolean sequentialRun(final long[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (Validation.isNotPrime(array[i])) {
+                hasNotPrime = true;
+                break;
+            }
+        }
+        //@return true if the number is not prime, else false
+        return hasNotPrime;
+    }
+}
